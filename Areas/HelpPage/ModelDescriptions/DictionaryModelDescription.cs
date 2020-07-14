9c2bf816baee88d67874d5449a080ac1148e7196
jadhavsantosh777 <jadhavsantosh777@gmail.com>
2020-07-14 21:37:19 +0530

    QRCodeReader

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace QRCodeReader.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QRCodeReader.WebApiApplication" Language="C#" %>
